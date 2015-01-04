ebab8fb6faee2980d1fea00cff7ca7cc70d03a91
tony vu <tony.vu@mytraining.net>
2015-01-04 18:22:23 -0500

    theme

--- wp-content/themes/skt_massage/index.php
+++ wp-content/themes/skt_massage/index.php
@@ -126,7 +126,13 @@ get_header(); ?>
 <div class="border-saprater"></div>
 
 <div class="home_content">
-	    <a href="http://www.pearlpureness.com.au/"><h3><img style="float:left;" src="http://acupunctureandbeautycentre.com.au/wp-content/uploads/2015/01/pearlpureness-logo.png" alt="pearlpureness"/><span style="margin-top:30px; margin-left:20px; color:#558c20; float:left">WELCOME TO PEARLPURENESS</span></h3></a>
+	<div style="float:left;">
+		<a href="http://www.pearlpureness.com.au/"><img style="float:left;" src="http://acupunctureandbeautycentre.com.au/wp-content/uploads/2015/01/pearlpureness-logo.png" alt="pearlpureness"/></a>
+	</div>
+	<div style="float:left; width:600px; margin-left:45px;">
+	    <a href="http://www.pearlpureness.com.au/"><h3 style="color:#558c20;">WELCOME TO PEARLPURENESS</h3></a>
+		<p>Pearl Pureness is the skincare range designed by Eli. It has an organic Australian skincare range and cosmeceutical range which are natural and contain no harsh chemicals or parabens.</p>
+	</div>
 <div class="clear"></div>
 </div><!--home_content -->
 <div class="clear"></div>
